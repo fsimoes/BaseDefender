302c2aa311a5bf59f74ef9b22f9fc2510824bf84
fsimoes <fsimoes@slctech.org>
2017-09-12 16:25:56 -0400

    Initial Commit

---
+++ Assets/Controllers/MouseController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    public GameObject circleCursor;
+
+    Vector3 lastFramePosition;
+    Vector3 currFramePosition;
+
+    Vector3 dragStartPostion;
+    List<GameObject> dragPreviewGameObjects;
+	// Use this for initialization
+	void Start () {
+        dragPreviewGameObjects = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        currFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currFramePosition.z = 0;
+        //update circle position
+        Tile tileUnderMouse = GetTileAtWorldCoord(currFramePosition);
+        if(tileUnderMouse != null)
+        {
+            circleCursor.SetActive(true);
+            Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
+            circleCursor.transform.position = cursorPosition;
+        }else
+        {
+            circleCursor.SetActive(false);
+        }
+
+        //Start Drag
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStartPostion = currFramePosition;
+        }
+        int start_x = Mathf.FloorToInt(dragStartPostion.x);
+        int end_x = Mathf.FloorToInt(currFramePosition.x);
+        if (end_x < start_x)
+        {
+            int temp = end_x;
+            end_x = start_x;
+            start_x = temp;
+
+        }
+        int start_y = Mathf.FloorToInt(dragStartPostion.y);
+        int end_y = Mathf.FloorToInt(currFramePosition.y);
+        if (end_y < start_y)
+        {
+            int temp = end_y;
+            end_y = start_y;
+            start_y = temp;
+
+        }
+        //Clean  old drag
+
+        while (dragPreviewGameObjects.Count > 0)
+        {
+            GameObject go = dragPreviewGameObjects[0];
+            dragPreviewGameObjects.RemoveAt(0);
+            SimplePool.Despawn(go);
+        }
+
+        //Display Drag Area
+        if (Input.GetMouseButton(0))
+        {
+            for (int x = start_x; x <= end_x; x++)
+            {
+                for (int y = start_y; y <= end_y; y++)
+                {
+                    Tile t = WorldController.Instance.World.GetTilesAt(x, y);
+                    if (t != null)
+                    {
+                        GameObject go = SimplePool.Spawn(circleCursor, new Vector3(x,y,0),Quaternion.identity);
+                        go.transform.SetParent(this.transform, true);
+                        dragPreviewGameObjects.Add(go);
+                    }
+
+                }
+            }
+
+        }
+
+        //Stop Drag
+        if (Input.GetMouseButtonUp(0))
+        {         
+          for (int x = start_x; x <= end_x; x++)
+            {
+                for (int y = start_y; y <= end_y; y++)
+                {
+                    Tile t = WorldController.Instance.World.GetTilesAt(x, y);
+                    if(t != null)
+                    {
+                        t.Type = Tile.TileType.Floor;
+                    }
+
+                }
+            }
+        }
+
+
+        //handle screen dragging
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(2)) //hold right or middle button
+        {
+            Vector3 diff = lastFramePosition - currFramePosition;
+            Camera.main.transform.Translate(diff);
+        }
+
+        Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize,3f,25f);
+
+        lastFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastFramePosition.z = 0;
+    }
+
+    Tile GetTileAtWorldCoord(Vector3 coord)
+    {
+        int x = Mathf.FloorToInt(coord.x);
+        int y = Mathf.FloorToInt(coord.y);
+
+        
+
+        return WorldController.Instance.World.GetTilesAt(x, y);
+    }
+}
---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WorldController : MonoBehaviour {
+
+    private static WorldController instance;
+
+    public Sprite floorSprite;
+
+    private World world;
+
+    public static WorldController Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+        protected set
+        {
+            instance = value;
+        }
+    }
+
+    public World World
+    {
+        get
+        {
+            return world;
+        }
+
+       protected set
+        {
+            world = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        if(Instance != null)
+        {
+            //error
+        }
+        Instance = this;
+        //create empty world
+        world = new World();
+        //Create GO to each tiles
+        for (int x = 0; x < world.Width; x++)
+        {
+            for (int y = 0; y < world.Height; y++)
+            {
+                Tile tile_data = world.GetTilesAt(x, y);
+
+                GameObject tile_go = new GameObject();
+                tile_go.name = "Tile_" + x + "_" + y;
+                tile_go.transform.position = new Vector3(tile_data.X,tile_data.Y,0);
+                tile_go.transform.SetParent(this.transform,true);
+                //create a empty sprite renderer
+                tile_go.AddComponent<SpriteRenderer>();
+
+                //lambda ()=> is equals to a empty function
+                tile_data.RegisterTileTypeChangedCallback((tile) => { OnTileTypeChanged(tile, tile_go); });
+            }
+        }
+
+        world.randomizeTitles();
+    }
+
+   
+	// Update is called once per frame
+	void Update () {
+      
+	}
+
+    void OnTileTypeChanged(Tile tile_data, GameObject tile_go)
+    {
+        if (tile_data.Type == Tile.TileType.Floor)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = floorSprite;
+        }else if(tile_data.Type == Tile.TileType.Empty)
+        {
+            tile_go.GetComponent<SpriteRenderer>().sprite = null;
+        }else
+        {
+            Debug.LogError("OnTileTypeChanged - Unrecognized tile type.");
+        }
+    }
+}
